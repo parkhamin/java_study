0516ddb34bae48cb411d46301625d3bd1df685a5
Hamin Park <parkhamint@naver.com>
2025-10-05 18:39:21 +0900

    조건문 - switch

---
+++ src/Chapter01/Switch.java
@@ -0,0 +1,56 @@
+package Chapter01;
+
+public class Switch {
+    public static void main(String[] args){
+        /*
+        A+ : 100
+        A  : 90 - 99
+        B  : 80 - 89
+        C  : 70 - 79
+        D  : 60 - 69
+        F  : 0 - 59
+         */
+        int score = 41;
+
+        String grade;
+
+        switch (score / 10){
+            case 10:
+                grade = "A+";
+                break;
+            case 9:
+                grade = "A";
+                break;
+            case 8:
+                grade = "B";
+                break;
+            case 7:
+                grade = "C";
+                break;
+            case 6:
+                grade = "D";
+                break;
+            default:
+                grade = "F";
+                break;
+        }
+        //System.out.println("당신의 학점은 " + grade + "입니다!! ㅊㅋㅊㅋ");
+
+        switch (grade){
+            case "A+":
+            case "A":
+            case "B":
+                System.out.println("참 잘했어요!");
+                break;
+            case "C":
+            case "D":
+                System.out.println("조금만 더 노력해 볼까요?");
+                break;
+            case "F":
+                System.out.println("Fail입니다.");
+            default:
+                System.out.println("다시 수강해주세요.");
+                break;
+        }
+    }
+}
---
+++ src/Chapter01/Switch_Ex01.java
@@ -0,0 +1,55 @@
+package Chapter01;
+
+public class Switch_Ex01 {
+    public static void main(String[] args){
+        int number = 1234567;
+
+        /*switch (number % 7){
+            case 0:
+                System.out.println("Zero");
+                break;
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            case 6:
+                System.out.println("Six");
+                break;
+        }*/
+
+        switch (number % 7){
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            case 6:
+                System.out.println("Six");
+                break;
+            default:
+                System.out.println("Zero");
+                break;
+        }
+    }
+}
