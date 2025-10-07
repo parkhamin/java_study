318da82be098741ce840408d9900b2725fc4a772
Hamin Park <parkhamint@naver.com>
2025-10-07 18:51:39 +0900

    숫자 도구

--- src/Chapter02/ClassMethod.java
+++ src/Chapter02/ClassMethod.java
@@ -1,9 +1,28 @@
 package Chapter02;
 
+import java.util.Random;
+
 public class ClassMethod {
     public static void main(String[] args){
         System.out.println(Math.abs(-10));
         System.out.println(Math.max(3, 7));
         System.out.println(Math.random()); // 0.0과 1.0 사이의 랜덤값
+
+        // 절댓값
+        System.out.println(Math.abs(-10));
+        System.out.println(Math.abs(8));
+
+        // 최솟값, 최댓값
+        System.out.println(Math.max(4, 10));
+        System.out.println(Math.min(4, 10));
+
+        // Random 클래서
+        Random r = new Random();
+        System.out.println(r.nextInt(10)); // 0 이상 9 이하의 랜덤한 값
+
+        // 10 이상 30 이하의 랜덤한 값
+        int min = 10;
+        int max = 30;
+        System.out.println(r.nextInt(max - min) + 1 + min);
     }
 }
