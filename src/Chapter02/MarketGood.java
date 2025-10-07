5990c0a430e6a8d94bb05b68fc97cb5be6e5e09b
Hamin Park <parkhamint@naver.com>
2025-10-07 18:04:19 +0900

    변수 안전하게 사용하기

--- src/Chapter02/MarketGood.java
+++ src/Chapter02/MarketGood.java
@@ -1,7 +1,7 @@
 public static class MarketGood {
     // 인스턴스 변수
-    private String name; // 상품의 이름
-    private int retailPrice; // 상품의 출시 가격
+    public final String name; // 상품의 이름
+    public final int retailPrice; // 상품의 출시 가격
     private int discountRate; // 상품의 할인율 (단위: %)
 
     // 생성자
@@ -19,13 +19,13 @@ public static class MarketGood {
     }
 
     // Getter와 Setter 메소드
-    public String getName(){
+    /*public String name{
         return name;
     }
 
     public int getRetailPrice(){
         return retailPrice;
-    }
+    }*/
 
     public void setDiscountRate(int discountRate){
         this.discountRate = discountRate;
@@ -46,11 +46,11 @@ public static void main(String[] args){
     MarketGood g2 = new MarketGood("MacBook Pro", 2990000, 15);
     MarketGood g3 = new MarketGood("iPhone 7", 920000, 20);
 
-    System.out.println(g1.getName() + "의 할인율: " + g1.getDiscountRate() + "%");
-    System.out.println(g2.getName() + "의 할인율: " + g2.getDiscountRate() + "%");
-    System.out.println(g3.getName() + "의 정상 가격: " + g3.getRetailPrice() + "원");
-    System.out.println(g3.getName() + "의 할인 가격: " + g3.getDiscountedPrice() + "원");
-    System.out.println("경쟁 업체가 많아져서 " + g3.getName() + "의 할인율이 35%로 올랐습니다.");
+    System.out.println(g1.name + "의 할인율: " + g1.getDiscountRate() + "%");
+    System.out.println(g2.name + "의 할인율: " + g2.getDiscountRate() + "%");
+    System.out.println(g3.name + "의 정상 가격: " + g3.retailPrice + "원");
+    System.out.println(g3.name + "의 할인 가격: " + g3.getDiscountedPrice() + "원");
+    System.out.println("경쟁 업체가 많아져서 " + g3.name + "의 할인율이 35%로 올랐습니다.");
     g3.setDiscountRate(35);
-    System.out.println(g3.getName() + "의 할인 가격: " + g3.getDiscountedPrice() + "원");
+    System.out.println(g3.name + "의 할인 가격: " + g3.getDiscountedPrice() + "원");
 }
