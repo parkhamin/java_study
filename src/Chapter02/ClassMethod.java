b19d91e57bee9fc5c409ca7b02becd485ea29f98
Hamin Park <parkhamint@naver.com>
2025-10-07 18:36:33 +0900

    클래스 메소드

---
+++ src/Chapter02/ClassMethod.java
@@ -0,0 +1,9 @@
+package Chapter02;
+
+public class ClassMethod {
+    public static void main(String[] args){
+        System.out.println(Math.abs(-10));
+        System.out.println(Math.max(3, 7));
+        System.out.println(Math.random()); // 0.0과 1.0 사이의 랜덤값
+    }
+}
